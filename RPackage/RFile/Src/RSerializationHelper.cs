5ec66b4e09a834e0fa55d9c1e679ad5671baff2e
Kabourlix <hdamaia@outlook.fr>
2023-10-06 20:40:11 +0200

    Adding OdinSerializer

---
+++ RPackage/RFile/Src/RSerializationHelper.cs
@@ -0,0 +1,27 @@
+﻿// Copyrighted by team Rézoskour
+// Created by Kabourlix Cendrée on 06
+
+#nullable enable
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rezoskour.RFile
+{
+    public static class RSerializationHelper
+    {
+        public static string ToJson<TKey,TValue>(this Dictionary<TKey,TValue> _dictionary)
+        {
+            StringBuilder sb = new("{");
+            foreach (KeyValuePair<TKey,TValue> kv in _dictionary)
+            {
+
+            }
+
+
+
+            sb.Append("}");
+            return sb.ToString();
+        }
+    }
+}
