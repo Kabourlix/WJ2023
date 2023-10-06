46077f32811d9059af50c5e468463ef2649bed6a
AlexandreBuzon <77610175+AlexandreBuzon@users.noreply.github.com>
2023-10-06 19:50:09 -0400

    Add Stats, PlayerStats, Xp system
    stats change if in berserk
    save stats
    xp curve

---
+++ RPackage/WonderJamContent/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,52 @@
+﻿// Copyrighted by team Rézoskour
+// Created by alexandre buzon on 06
+
+#nullable enable
+
+using System;
+using UnityEngine;
+
+// Copyrighted by team Rézoskour
+// Created by alexandre buzon on 06
+
+namespace Rezoskour.Content
+{
+    public class PlayerStats : MonoBehaviour
+    {
+        [SerializeField] private Stats? baseStats;
+        private Stats? normalStats;
+        private Stats? currentStats;
+        private bool isBerserk;
+
+        private void Start()
+        {
+            if (baseStats == null)
+            {
+                throw new Exception("Stats not set in PlayerStats");
+            }
+
+            //baseStats.SaveStatsToJson();
+            currentStats = Instantiate(baseStats);
+            //currentStats.LoadStatsFromJson();
+        }
+
+        private void SwitchMode(bool _isBerserk)
+        {
+            if (currentStats == null)
+            {
+                throw new Exception("Stats not set in PlayerStats");
+            }
+
+            if (_isBerserk)
+            {
+                Destroy(normalStats);
+                normalStats = Instantiate(currentStats);
+                currentStats.BerserkMode();
+            }
+            else
+            {
+                currentStats = normalStats;
+            }
+        }
+    }
+}
---
+++ RPackage/WonderJamContent/Scripts/Stats/Stats.cs
@@ -0,0 +1,84 @@
+﻿// Copyrighted by team Rézoskour
+// Created by alexandre buzon on 06
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using Rezoskour.OdinSerializer;
+using UnityEngine;
+
+// Copyrighted by team Rézoskour
+// Created by alexandre buzon on 06
+
+namespace Rezoskour.Content
+{
+    [CreateAssetMenu(fileName = "Stats", menuName = "Rézoskour/Stats Data", order = 0)]
+    public class Stats : SerializedScriptableObject
+    {
+        [SerializeField] private int health;
+        public int Health => health;
+
+        [SerializeField] private int maxHealth;
+        public int MaxHealth => maxHealth;
+
+        [SerializeField] private int oil;
+        public int Oil => oil;
+
+        [SerializeField] private int maxOil;
+        public int MaxOil => maxOil;
+
+        [SerializeField] private int attack;
+        public int Attack => attack;
+
+        [SerializeField] private int collectRange;
+        public int CollectRange => collectRange;
+
+        [SerializeField] private int range;
+        public int Range => range;
+
+        [SerializeField] private int speed;
+        public int Speed => speed;
+
+        public void SaveStatsToJson()
+        {
+            var jsonStr = JsonUtility.ToJson(this);
+            /*string filePath = Application.persistentDataPath+"/stats.json";
+            System.IO.File.WriteAllText(filePath, jsonStr);
+            Debug.Log("Stats saved.");*/
+
+            PlayerPrefs.SetString("Stats", jsonStr);
+            PlayerPrefs.Save();
+            Debug.Log("Stats saved.");
+        }
+
+        public bool LoadStatsFromJson()
+        {
+            var jsonString = PlayerPrefs.GetString("Stats", "");
+            Debug.Log("ALEX : " + jsonString);
+            if (jsonString is "" or "{}" or "null")
+            {
+                return false;
+            }
+
+            JsonUtility.FromJsonOverwrite(jsonString, this);
+            return true;
+        }
+
+        public void LoadStats()
+        {
+            var filePath = Application.persistentDataPath + "/stats.json";
+            var jsonString = System.IO.File.ReadAllText(filePath);
+
+            JsonUtility.FromJsonOverwrite(jsonString, this);
+        }
+
+        public void BerserkMode()
+        {
+            attack *= 3;
+            collectRange *= 3;
+            range *= 3;
+            speed *= 3;
+        }
+    }
+}
