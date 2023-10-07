a61f209435e62476462966b8bdab155a3d9ad0ea
Kabourlix <hdamaia@outlook.fr>
2023-10-07 22:30:11 +0200

    Update Stats and start working on perks

---
+++ RPackage/WonderJamContent/Scripts/Stats/Perks/PerksData.cs
@@ -0,0 +1,20 @@
+﻿// Copyrighted by team Rézoskour
+// Created by Kabourlix Cendrée on 07
+
+using UnityEngine;
+
+namespace Rezoskour.Content.Perks
+{
+    [CreateAssetMenu(fileName = "Perks", menuName = "Rezoskour/PerksData", order = 0)]
+    public class PerksData : ScriptableObject
+    {
+        [SerializeField] private StatName statName;
+        public StatName StatName => statName;
+
+        [SerializeField] private float multiplier;
+        public float Multiplier => multiplier;
+
+        [SerializeField] private float addValue;
+        public float AddValue => addValue;
+    }
+}
---
+++ RPackage/WonderJamContent/Scripts/Stats/Perks/PerksUpdateManager.cs
@@ -0,0 +1,11 @@
+﻿// Copyrighted by team Rézoskour
+// Created by Kabourlix Cendrée on 07
+
+#nullable enable
+
+using UnityEngine;
+
+namespace Rezoskour.Content.Perks
+{
+    public class PerksUpdateManager : MonoBehaviour { }
+}
