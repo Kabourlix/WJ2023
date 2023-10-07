a799b24a5f66b9460849a299ae95dd2b27e00901
Kabourlix <hdamaia@outlook.fr>
2023-10-07 16:42:45 +0200

    Add Helper for layerMask

---
+++ RPackage/WonderJamContent/Scripts/Misc/Helper.cs
@@ -0,0 +1,25 @@
+﻿// Copyrighted by team Rézoskour
+// Created by Kabourlix Cendrée on 07
+
+#nullable enable
+
+using UnityEngine;
+
+// Copyrighted by team Rézoskour
+// Created by Kabourlix Cendrée on 07
+
+namespace Rezoskour.Content.Misc
+{
+    public static class Helper
+    {
+        public static bool HasNot(this LayerMask _layerMask, int _layer)
+        {
+            return ((1 << _layer) & _layerMask) == 0;
+        }
+
+        public static bool Has(this LayerMask _layerMask, int _layer)
+        {
+            return ((1 << _layer) & _layerMask) == _layerMask;
+        }
+    }
+}
