e61b1b2cbd231a8d9e73ad6be16e60814264972d
Kabourlix <hdamaia@outlook.fr>
2023-10-07 23:07:36 +0200

    Update crate

--- RPackage/WonderJamContent/Scripts/Collectable/Crate.cs
+++ RPackage/WonderJamContent/Scripts/Collectable/Crate.cs
@@ -1,5 +1,5 @@
 ﻿// Copyrighted by team Rézoskour
-// Created by alexandre buzon on 07
+// Created by Kabourlix Cendrée on 07
 
 #nullable enable
 
@@ -15,34 +15,43 @@ namespace Rezoskour.Content.Collectable
         [Range(0, 1)] [SerializeField] private float oilSpawnProbability;
         [Range(0, 1)] [SerializeField] private float expSpawnProbability;
 
+        [SerializeField] private int hp = 100;
+
         [ContextMenu("Destroy")]
         private void DestoryCrate()
         {
+            if (CollectableManager.Instance == null)
+            {
+                Debug.LogError("CollectableManager.Instance is null !");
+                return;
+            }
+
             Debug.Log("Crate destroyed");
-            var range = oilSpawnProbability + expSpawnProbability;
-            var rand = Random.Range(0, range);
+            float range = oilSpawnProbability + expSpawnProbability;
+            float rand = Random.Range(0, range);
             if (rand <= oilSpawnProbability)
             {
-                var transform1 = transform;
+                Transform transform1 = transform;
                 CollectableManager.Instance.SpawnOil(transform1.position, transform1.rotation);
             }
             else
             {
-                var transform1 = transform;
+                Transform transform1 = transform;
                 CollectableManager.Instance.SpawnXp(transform1.position, transform1.rotation);
             }
 
             Destroy(gameObject);
         }
 
-        public void Heal(int _amount)
-        {
-            throw new System.NotImplementedException();
-        }
+        public void Heal(int _amount) { }
 
         public void Damage(int _amount)
         {
-            DestoryCrate();
+            hp -= _amount;
+            if (hp <= 0)
+            {
+                DestoryCrate();
+            }
         }
     }
 }
