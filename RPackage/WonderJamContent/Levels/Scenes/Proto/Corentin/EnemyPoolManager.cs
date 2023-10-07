e07a58b342f33eab38b5a20a995eb96fdae9394c
Kabourlix <hdamaia@outlook.fr>
2023-10-07 23:26:11 +0200

    Pool for enemies

--- RPackage/WonderJamContent/Levels/Scenes/Proto/Corentin/EnemyPoolManager.cs
+++ RPackage/WonderJamContent/Levels/Scenes/Proto/Corentin/EnemyPoolManager.cs
@@ -1,45 +1,73 @@
+// Copyrighted by team Rézoskour
+// Created by Kabourlix Cendrée on 07
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Pool;
+using UnityEngine.Serialization;
 
 namespace Rezoskour.Content
 {
+    public enum EnemyType
+    {
+        Fries,
+        Ketchup
+    }
     public class EnemyPoolManager : MonoBehaviour
     {
-        [SerializeField] private ChasingEnemy enemyPrefab = null!;
-        private ObjectPool<ChasingEnemy> enemyPool = null!;
+        [FormerlySerializedAs("ketchupPrefab")] [SerializeField] private ChasingEnemy friesPrefab = null!;
+        [SerializeField] private ChasingEnemy ketchupPrefab = null!;
 
+        private ObjectPool<ChasingEnemy> enemyPool = null!;
+        private Dictionary<EnemyType, ObjectPool<ChasingEnemy>> enemyPools = new();
         private void Awake()
         {
-            enemyPool = new ObjectPool<ChasingEnemy>(OnCreateEnemy, OnGetEnemy, OnReleaseEnemy);
+            //enemyPools.Add(Fries, instanciation de la pool);
+            enemyPool = new ObjectPool<ChasingEnemy>(()=>OnCreateEnemyAbstract(friesPrefab), OnGetEnemy, OnReleaseEnemy);
         }
-        
 
-        private ChasingEnemy OnCreateEnemy()
+
+        private ChasingEnemy OnCreateEnemyAbstract(ChasingEnemy _prefab)
         {
             if (GameManager.Instance == null)
             {
                 Debug.LogError("GAMEMANAGER IS NULL!");
                 return default!;
             }
-            ChasingEnemy enemy = Instantiate(enemyPrefab, GameManager.Instance.transform).GetComponent<ChasingEnemy>();
+
+            ChasingEnemy enemy = Instantiate(_prefab, GameManager.Instance.transform).GetComponent<ChasingEnemy>();
             enemy.name = $"Enemy {enemyPool.CountAll + 1}";
             enemy.Init(() => enemyPool.Release(enemy));
             return enemy;
         }
 
+        // private ChasingEnemy OnCreateEnemy()
+        // {
+        //     if (GameManager.Instance == null)
+        //     {
+        //         Debug.LogError("GAMEMANAGER IS NULL!");
+        //         return default!;
+        //     }
+        //
+        //     ChasingEnemy enemy = Instantiate(friesPrefab, GameManager.Instance.transform).GetComponent<ChasingEnemy>();
+        //     enemy.name = $"Enemy {enemyPool.CountAll + 1}";
+        //     enemy.Init(() => enemyPool.Release(enemy));
+        //     return enemy;
+        // }
+
         private void OnGetEnemy(ChasingEnemy _enemy)
         {
-           _enemy.gameObject.SetActive(true);
+            _enemy.gameObject.SetActive(true);
         }
 
         private void OnReleaseEnemy(ChasingEnemy _enemy)
         {
             _enemy.gameObject.SetActive(false);
         }
+
 
         public ChasingEnemy GetEnemy()
         {
--- RPackage/WonderJamContent/Levels/Scenes/Proto/Corentin/SpawnerEnemy.cs
+++ RPackage/WonderJamContent/Levels/Scenes/Proto/Corentin/SpawnerEnemy.cs
@@ -1,3 +1,5 @@
+// Copyrighted by team Rézoskour
+// Created by Kabourlix Cendrée on 07
 
 using System;
 using Unity.Mathematics;
@@ -8,39 +10,43 @@ namespace Rezoskour.Content
 {
     public class EnemySpawner : MonoBehaviour
     {
-        public EnemyPoolManager enemyPoolManager; 
-        public float spawnInterval = 2.0f; 
+        public EnemyPoolManager enemyPoolManager;
+        public float spawnInterval = 2.0f;
         private Camera playerCamera;
         private GameObject player;
         private float lastSpawnTime;
-        private Random random = new Random(1);
+        private Random random = new(1);
 
         private void Awake()
         {
-            random = new Random((uint)System.Environment.TickCount);
+            random = new Random((uint) Environment.TickCount);
         }
 
         private void Start()
         {
-            player = FindObjectOfType<PlayerMovement>().gameObject;
+            if (GameManager.Instance == null)
+            {
+                Debug.LogError("GameManager.Instance is null !");
+                return;
+            }
+
+            player = GameManager.Instance.PlayerTf.gameObject;
             lastSpawnTime = Time.time;
             playerCamera = player.GetComponentInChildren<Camera>();
         }
 
         private void Update()
         {
-            
             if (Time.time - lastSpawnTime >= spawnInterval)
             {
                 SpawnEnemy();
                 lastSpawnTime = Time.time;
             }
         }
-        
+
 
         private void SpawnEnemy()
         {
-            
             float cameraHeight = playerCamera.orthographicSize;
             float cameraWidth = cameraHeight * playerCamera.aspect;
             float2 spawnPosition;
@@ -48,21 +54,25 @@ namespace Rezoskour.Content
             if (random.NextBool())
             {
                 // Moitié supérieure
-                spawnPosition = random.NextFloat2(new float2(cameraPosition.x - cameraWidth, cameraPosition.y + cameraWidth), new float2(cameraPosition.y, cameraPosition.y + cameraHeight));
+                spawnPosition =
+                    random.NextFloat2(new float2(cameraPosition.x - cameraWidth, cameraPosition.y + cameraWidth),
+                        new float2(cameraPosition.y, cameraPosition.y + cameraHeight));
             }
             else
             {
                 // Moitié inférieure
-                spawnPosition = random.NextFloat2(new float2(cameraPosition.x - cameraWidth, cameraPosition.x + cameraWidth), new float2(cameraPosition.y - cameraHeight, cameraPosition.y));            }
-            Vector3 spawnPoint = new Vector3(spawnPosition.x, spawnPosition.y, 0f);
+                spawnPosition =
+                    random.NextFloat2(new float2(cameraPosition.x - cameraWidth, cameraPosition.x + cameraWidth),
+                        new float2(cameraPosition.y - cameraHeight, cameraPosition.y));
+            }
+
+            Vector3 spawnPoint = new(spawnPosition.x, spawnPosition.y, 0f);
             ChasingEnemy enemy = enemyPoolManager.GetEnemy();
 
             if (enemy != null)
             {
                 enemy.transform.position = spawnPoint;
             }
-            
         }
-        
     }
 }
